1564fd2ed7ff18c3a7b5eb16c20607491f0e9446
Jesus Hernández Martínez <jesus_hdezmtz@hotmail.com>
2020-11-04 15:41:13 -0600

    Creación de rama Jesús
    Se creó la rama Jesuús, además se agrega una nueva vista para el contacto

---
+++ arcup/src/views/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ texto }}</h1>
+    <input v-model="message" placeholder="Nombre" />
+    <input v-model="message" placeholder="Telefono" />
+    <input v-model="message" placeholder="Correo" />
+    <select v-model="selected">
+      <option disabled value="">Seleccione una ciudad</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <select v-model="selected">
+      <option disabled value="">Seleccione un Estado</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <input v-model="message" placeholder="Nombre del proyecto" />
+    <input v-model="message" placeholder="Descripcion del proyecto" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Contactanos",
+    };
+  },
+};
+</script>
+
