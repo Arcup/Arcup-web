dd311fd413101476780326c9298aa9f8544426c9
Jesus Hernández Martínez <jesus_hdezmtz@hotmail.com>
2020-11-06 15:39:28 -0600

    Vuetify: se agregó vuetify al proyecto
    Se agregó todo lo necesario para utilizar vuetify, así como un ejemplo en el archivo app.vue

--- arcup/src/views/Contact.vue
+++ arcup/src/views/Contact.vue
@@ -1,31 +1,56 @@
 <template>
-  <div>
-    <h1>{{ texto }}</h1>
-    <input v-model="message" placeholder="Nombre" />
-    <input v-model="message" placeholder="Telefono" />
-    <input v-model="message" placeholder="Correo" />
-    <select v-model="selected">
-      <option disabled value="">Seleccione una ciudad</option>
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-    </select>
-    <select v-model="selected">
-      <option disabled value="">Seleccione un Estado</option>
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-    </select>
-    <input v-model="message" placeholder="Nombre del proyecto" />
-    <input v-model="message" placeholder="Descripcion del proyecto" />
-  </div>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: "Contactanos",
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
     };
   },
 };
