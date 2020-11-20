ab605a37dacc4dc359ad3608b5e2bdc24af724aa
Valdivieso <jahiirvaldivieso@hotmail.com>
2020-11-19 20:27:35 -0600

    Update versión movil
    Actualización responsiva para la versión movil

--- arcup/src/views/Technology.vue
+++ arcup/src/views/Technology.vue
@@ -8,8 +8,7 @@
       max-height="200"
     >
     </v-img>
-
-    <v-container>
+    <v-container v-show="!isMobile()">
       <v-row>
         <v-col class="" cols="12" sm="4" md="4" lg="4">
           <v-hover v-slot="{ hover }" open-delay="200" class="centrado">
@@ -19,15 +18,15 @@
               :class="{ 'on-hover': hover }"
               class=""
               height="100"
-              max-width="100"              
+              max-width="100"
             >
-              <v-avatar size="100" class="background" @click="selec1">
+              <v-avatar size="100" class="background" @click="selecWeb">
                 <v-icon size="60" dark> mdi-web </v-icon>
               </v-avatar>
             </v-card>
-          </v-hover>          
+          </v-hover>
         </v-col>
-        
+
         <v-col class="text-center" cols="12" sm="4" md="4" lg="4">
           <v-hover v-slot="{ hover }" open-delay="200" class="centrado">
             <v-card
@@ -37,15 +36,14 @@
               class=""
               height="100"
               max-width="100"
-              
             >
-              <v-avatar size="100" class="background" @click="selec2">
+              <v-avatar size="100" class="background" @click="selecAndroid">
                 <v-icon size="60" dark>mdi-android</v-icon>
               </v-avatar>
             </v-card>
           </v-hover>
         </v-col>
-        <v-col class="text-center"  cols="12" sm="4" md="4" lg="4">
+        <v-col class="text-center" cols="12" sm="4" md="4" lg="4">
           <v-hover v-slot="{ hover }" open-delay="200" class="centrado">
             <v-card
               rounded="circle"
@@ -55,20 +53,17 @@
               height="100"
               max-width="100"
             >
-              <v-avatar size="100" class="background" @click="selec3">
+              <v-avatar size="100" class="background" @click="selecConsultation">
                 <v-icon size="50" dark> mdi-account-group </v-icon>
               </v-avatar>
             </v-card>
           </v-hover>
         </v-col>
       </v-row>
-    </v-container>
-
-    <div>
       <v-col class="shrink">
         <v-scroll-x-transition hide-on-leave>
           <v-card
-            v-show="expand"
+            v-show="expandWeb"
             height="200"
             width="70%"
             class="mx-auto"
@@ -79,7 +74,7 @@
         </v-scroll-x-transition>
         <v-fade-transition hide-on-leave>
           <v-card
-            v-show="expand2"
+            v-show="expandAndroid"
             height="200"
             width="70%"
             class="mx-auto"
@@ -90,7 +85,7 @@
         </v-fade-transition>
         <v-scroll-x-reverse-transition hide-on-leave>
           <v-card
-            v-show="expand3"
+            v-show="expandConsultation"
             height="200"
             width="70%"
             class="mx-auto"
@@ -100,7 +95,105 @@
           </v-card>
         </v-scroll-x-reverse-transition>
       </v-col>
+
+    </v-container>
+
+    <div>
+      
     </div>
+
+    <!-- Versión movil -->
+    <div v-show="isMobile()">
+      <v-row justify="center" cols="12" sm="4" md="4" lg="4">
+        <v-hover v-slot="{ hover }" open-delay="200" class="">
+          <v-card
+            rounded="circle"
+            :elevation="hover ? 16 : 2"
+            :class="{ 'on-hover': hover }"
+            class=""
+            height="100"
+            max-width="100"
+          >
+            <v-avatar size="100" class="background" @click="expandWeb = true">
+              <v-icon size="60" dark> mdi-web </v-icon>
+            </v-avatar>
+          </v-card>
+        </v-hover>
+      </v-row>
+
+      <v-fade-transition hide-on-leave>
+        <v-card
+          v-show="expandWeb"
+          height="200"
+          width="70%"
+          class="mx-auto"
+          color="blue"
+        >
+          111111111111111
+        </v-card>
+      </v-fade-transition>
+
+      <v-row justify="center" cols="12" sm="4" md="4" lg="4">
+        <v-hover v-slot="{ hover }" open-delay="200" class="">
+          <v-card
+            rounded="circle"
+            :elevation="hover ? 16 : 2"
+            :class="{ 'on-hover': hover }"
+            class=""
+            height="100"
+            max-width="100"
+          >
+            <v-avatar size="100" class="background" @click="expandAndroid = true">
+              <v-icon size="60" dark>mdi-android</v-icon>
+            </v-avatar>
+          </v-card>
+        </v-hover>
+      </v-row>
+
+      <v-fade-transition hide-on-leave>
+        <v-card
+          v-show="expandAndroid"
+          height="200"
+          width="70%"
+          class="mx-auto"
+          color="blue"
+        >
+          22222222222
+        </v-card>
+      </v-fade-transition>
+
+      <v-row justify="center" cols="12" sm="4" md="4" lg="4">
+        <v-hover v-slot="{ hover }" open-delay="200" class="">
+          <v-card
+            rounded="circle"
+            :elevation="hover ? 16 : 2"
+            :class="{ 'on-hover': hover }"
+            class=""
+            height="100"
+            max-width="100"
+          >
+            <v-avatar size="100" class="background" @click="expandConsultation = true">
+              <v-icon size="50" dark> mdi-account-group </v-icon>
+            </v-avatar>
+          </v-card>
+        </v-hover>
+      </v-row>
+
+      <v-fade-transition hide-on-leave>
+        <v-card
+          v-show="expandConsultation"
+          height="200"
+          width="70%"
+          class="mx-auto"
+          color="blue"
+        >
+          333333333333333333333
+        </v-card>
+      </v-fade-transition>
+    </div>
+
+    <!-- FInaliza versión movil-->
+
     <base-footer> </base-footer>
   </div>
 </template>
@@ -115,33 +208,36 @@ export default {
   },
   data() {
     return {
-      expand: true,
-      expand2: false,
-      expand3: false,
+      expandWeb: true,
+      expandAndroid: false,
+      expandConsultation: false,
     };
   },
   methods: {
-    selec1: function () {
-      this.expand2 = false;
-      this.expand3 = false;
-      this.expand = true;
+    selecWeb: function () {
+      this.expandAndroid = false;
+      this.expandConsultation = false;
+      this.expandWeb = true;
     },
-    selec2: function () {
-      this.expand = false;
-      this.expand3 = false;
-      this.expand2 = true;
+    selecAndroid: function () {
+      this.expandWeb = false;
+      this.expandConsultation = false;
+      this.expandAndroid = true;
     },
-    selec3: function () {
-      this.expand = false;
-      this.expand2 = false;
-      this.expand3 = true;
+    selecConsultation: function () {
+      this.expandWeb = false;
+      this.expandAndroid = false;
+      this.expandConsultation = true;
+    },
+    isMobile: function () {
+      return this.$vuetify.breakpoint.xlOnly || this.$vuetify.breakpoint.xsOnly;
     },
   },
 };
 </script>
 <style lang='css'>
 .centrado {
-    margin-left: 33% !important;
+  margin-left: 33% !important;
 }
 #imagenTecnologias {
   margin-bottom: 30px;
