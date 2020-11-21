d23949cb2631e0659665803ac992cffe21a88204
Jesus Hernández Martínez <jesus_hdezmtz@hotmail.com>
2020-11-21 09:32:08 -0600

    Se agregó la tarjeta de prueba

--- arcup/src/views/About.vue
+++ arcup/src/views/About.vue
@@ -83,6 +83,35 @@
       </v-col>
     </v-row>
   </v-container>
+  <v-container>
+    <div>
+      <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <div class="text-center">Paquete</div>
+      <p class="display-1  text-center font__main-alter">
+        Informativo
+      </p>
+      <p>adjective</p>
+      <div class="text--primary">
+        well meaning and kindly.<br>
+        "a benevolent smile"
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+      
+    </div>
+  </v-container>
   <base-footer> </base-footer>
 </div>
 </template>
