681e4143a9122728432cb328417087a40eb97654
Valdivieso <jahiirvaldivieso@hotmail.com>
2020-11-28 02:49:32 -0600

    Actualización basefooter
    Se agregaron números de teléfono al basefooter
    caracteristicas de las técnologías
    imagenes prueba para las cards de técnologías

--- arcup/src/components/BaseFooter.vue
+++ arcup/src/components/BaseFooter.vue
@@ -1,88 +1,100 @@
 <template>
   <div>
-    <v-footer color="#161E2E" >
-      <v-card
-        color="#161E2E"
-        width="100%"
-        tile
-        class="lighten-1 white--text text-center"
-        flat
-      >
-        <v-card-text class="d-flex justify-center pa-0"> 
-          <figure class="icon__arcup"></figure>          
-        </v-card-text>
+    <v-footer color="#161E2E">
+      <v-row>
+        <v-col class="ma-0 " cols="12" sm="12" md="4" lg="4" xl="4">
+          <v-card-text class="text-center white--text">
+            <div class="body-1">Correo: contacto@arcup.com.mx</div>
+            <div class="body-1">Telefonos: 2123123212</div>
+            <div class="body-2">2123123212 </div>
+            <div class="body-2">2123123212 </div>
+          </v-card-text>
+        </v-col>
 
-        <v-card-text  class="d-flex justify-center">
-          <div>
-            <v-btn
-              target="_blank"
-              href="https://www.facebook.com/arcupdev"
-              class="mx-4 white--text"
-              icon
-            >
-              <v-icon color="whithe" size="30px"> 
-                mdi-facebook 
-              </v-icon>
-            </v-btn>
-            <h5>Arcup</h5>
-          </div>
-          <div>
-            <v-btn
-              target="_blank"
-              href="https://www.instagram.com/arcupdev/"
-              class="mx-4 white--text"
-              icon
-            >
-              <v-icon color="whithe" size="30px"> 
-                mdi-instagram 
-              </v-icon>
-            </v-btn>
-            <h5>Instagram</h5>
-          </div>
-          <div>
-            <v-btn
-              target="_blank"
-              href="https://twitter.com/arcupdev"
-              class="mx-4 white--text"
-              icon
-            >
-              <v-icon color="whithe" size="30px"> mdi-twitter </v-icon>
-            </v-btn>
-            <h5>Twitter</h5>
-          </div>
-          <div>
-            <v-btn
-              target="_blank"
-              href="https://vuetifyjs.com/en/api/v-btn/#v-btn-api"
-              class="mx-4 white--text"
-              icon
-            >
-              <v-icon color="whithe" size="30px"> mdi-linkedin </v-icon>
-            </v-btn>
-            <h5>Linkedin</h5>
-          </div>
-        </v-card-text>
-      </v-card>
+        <v-col class="ma-0 " cols="12" sm="12" md="4" lg="4" xl="4">
+          <v-row no-gutters class="ml-4 mr-2 mb-2">
+            <v-col class="ma-0 pa-0" cols="12" sm="3" md="3" lg="3" xl="3">
+              <v-card-text class="d-flex justify-center pr-0">
+                <v-img
+                  max-height="54"
+                  max-width="40"
+                  src="../assets/formaLogo-Arcup.svg"
+                ></v-img>
+              </v-card-text>
+            </v-col>
+            <v-col cols="12" sm="7" md="7" lg="7" xl="7">
+              <v-card-text id="content" class="text-justify body-2 white--text pa-1">
+                En Arcup nos comprometemos con la entrega puntual del producto y
+                te brindamos soporte las 24 horas los 7 días de la semana
+              </v-card-text>              
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col class="ma-0 " cols="12" sm="12" md="4" lg="4" xl="4">
+          <v-card color="#161E2E" width="100%" tile class="text-center" flat>
+            <v-card-text class="d-flex justify-center">
+              <div v-for="(item, index) in redesSociales" :key="index">
+                <v-btn
+                  target="_blank"
+                  :href="item.href"
+                  class="mx-4 white--text"
+                  icon
+                >
+                  <v-icon color="whithe" size="34px"> {{ item.icon }} </v-icon>
+                </v-btn>
+                <div class="body-2 white--text">{{ item.name }}</div>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-footer>
   </div>
 </template>
-
 <script>
 export default {
-  data: () => ({
-    icons: ["mdi-facebook", "mdi-instagram", "mdi-twitter", "mdi-linkedin"],
-  }),
+  data() {
+    return {
+      redesSociales: [
+        {
+          name: "Facebook",
+          icon: "mdi-facebook",
+          href: "https://www.facebook.com/arcupdev",
+        },
+        {
+          name: "Instagram",
+          icon: "mdi-instagram",
+          href: "https://www.instagram.com/arcupdev/",
+        },
+        {
+          name: "Twitter",
+          icon: "mdi-twitter",
+          href: "https://twitter.com/arcupdev",
+        },
+        {
+          name: "Linkedin",
+          icon: "mdi-linkedin",
+          href: "https://vuetifyjs.com/en/api/v-btn/#v-btn-api",
+        },
+      ],
+    };
+  },
 };
 </script>
-
 <style lang='css'>
-  h5 {
-    color: rgb(255, 235, 205);
-    font-family: GilroyLight;
-  }
-  .icon__arcup {
-  background-image: url("../assets/formaLogo-Arcup.svg");
-  width: 45px;
-  height: 45px;  
+h5 {
+  color: #00ffff;
+  font-family: GilroyLight;
 }
-</style>
+#text {
+  margin-left: 15%;
+  margin-right: 15%;
+  margin-bottom: 10px;
+  text-align: justify;
+}
+#content{
+  max-width: 360px;
+}
+</style> 
+ <!--../assets/formaLogo-Arcup.svg -->
--- arcup/src/views/Software.vue
+++ arcup/src/views/Software.vue
@@ -10,21 +10,21 @@
     >
     </v-img>
     <div v-for="(item, index) in sections" :key="index">
-      <v-card class="text-center" height="150">
-        <h2>{{ item.title }}</h2>
-        <v-card-text class="text-start">{{ item.description }}</v-card-text>
+      <v-card flat class="text-center" width="100%" height="140">
+        <h2>{{ item.title }}</h2>        
+        <v-card-text class="text-center">{{ item.description }}</v-card-text>        
       </v-card>
-
-      <v-parallax dark :src="item.src" height="270" class="mb-16">        
+      
+      <v-parallax dark :src="item.src" height="300" class="mb-16">        
       </v-parallax>
     </div>
-
     <base-footer> </base-footer>
   </div>
 </template>
 <script>
 import BaseHeader from "@/components/BaseHeader.vue";
 import BaseFooter from "@/components/BaseFooter.vue";
+
 export default {
   name: "Software",
   components: {
@@ -36,33 +36,32 @@ export default {
       sections: [
         {
           title: "Obtención de requisitos",
-          description: "Compatibilidad con los navegadores",
+          description: "En Arcup organizamos reuniones con nuestros clientes para descubrir y entender tus necesidades.",
           src:
             "https://cdn.pixabay.com/photo/2017/01/14/10/56/men-1979261_960_720.jpg",
         },
         {
           title: "Análisis",
-          description: "Compatibilidad con los navegadores",
+          description: "Revisamos las necesidades expuestas para determinar los requerimientos y generar una propuesta de solución que cumpla tus expectativas.",
           src:
             "https://cdn.pixabay.com/photo/2019/03/07/16/48/hands-4040619_960_720.jpg",
         },
         {
           title: "Diseño",
-          description: "Compatibilidad con los navegadores",
+          description: "Elaboramos prototipos que te permitirán conocer una vista del contenido de tu sistema, también, elegimos las mejores tecnologías y herramientas para su desarrollo.",
           src:
             "https://cdn.pixabay.com/photo/2015/09/29/22/49/blueprint-964629_960_720.jpg",
         },
         {
           title: "Codificación",
-          description: "Compatibilidad con los navegadores",
-          src:
-            "https://cdn.pixabay.com/photo/2019/06/17/19/48/source-4280758_960_720.jpg",
+          description: "Codificamos el software por iteraciones para mostrarte los avances que se han realizado. ",
+          src: "https://cdn.pixabay.com/photo/2019/03/07/16/48/hands-4040619_960_720.jpg",
         },
         {
           title: "Pruebas",
-          description: "Compatibilidad con los navegadores",
+          description: "Realizamos pruebas en paralelo para garantizar el correcto funcionamiento de tú proyecto. Al finalizar desplegamos tú sistema para que valides su funcionamiento.",
           src:
-            "https://ichef.bbci.co.uk/news/640/cpsprodpb/113AC/production/_113427507_solar_orbiter_eui-fullsun01.jpg",
+            "https://cdn.pixabay.com/photo/2020/05/22/21/44/review-5207277_960_720.jpg",
         },
       ],
     };
